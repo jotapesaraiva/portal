9b9afaf8fea2ea10b63507844149e6f41d6fd550
joao.saraiva <joao.saraiva@sefa.pa.gov.br>
2018-06-13 12:19:29 -0300

    add method consumo_unidade in controller consumo_banda

--- application/controllers/links/Consumo_banda.php
+++ application/controllers/links/Consumo_banda.php
@@ -112,16 +112,230 @@ class Consumo_banda extends CI_Controller {
 
     public function consumo_unidade($unidade) {
 
+      $css['headerinc'] = '
+          <link href="' . base_url() . 'assets/custom/bootstrap-select/dist/css/bootstrap-select.css" rel="stylesheet" type="text/css">
+          <link href="' . base_url() . 'assets/global/plugins/datatables/datatables.min.css" rel="stylesheet" type="text/css" />
+          <link href="' . base_url() . 'assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css" rel="stylesheet" type="text/css" />';
+
+      $script['footerinc'] = '
+          <script src="' . base_url() . 'assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
+          <script src="' . base_url() . 'assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
+          <script src="' . base_url() . 'assets/custom/historico.js" type="text/javascript"></script>
+          <script src="' . base_url() . 'assets/custom/bootstrap-select/dist/js/bootstrap-select.js"></script>';
+      $script['script'] = '';
+
+        $session['username'] = $this->session->userdata('username');
+
+        $this->breadcrumbs->unshift('<i class="icon-home"></i> Home', 'portal');
+        $this->breadcrumbs->push('<span>Link</span>','/link');
+        $this->breadcrumbs->push('<span>Consumo de Banda</span>','link/consumo_banda');
+
+        $data['consumo'] = $this->table_unidade($unidade);
+
+        $this->load->view('template/header',$css);
+        $this->load->view('template/navbar',$session);
+        $this->load->view('template/sidebar');
+
+        $this->load->view('link/consumo_unidade',$data);
+
+        $this->load->view('template/footer',$script);
+
+    }
+
+    public function table_unidade($unidade) {
+      $consumoAtual = $this->consumo_model->consumo_unidade($unidade);
+      $consumo1h = $this->consumo_model->consumo_unidade($unidade,'1 HOUR');
+      $consumo6h = $this->consumo_model->consumo_unidade($unidade, '6 HOUR');
+      $consumo24h = $this->consumo_model->consumo_unidade($unidade, '24 HOUR');
+      $consumo7d = $this->consumo_model->consumo_unidade($unidade, '7 DAY');
+      $consumo30d = $this->consumo_model->consumo_unidade($unidade, '30 DAY');
+        $html ="";
+        $html.="<tr>";
+        $html.="<td>".$consumoAtual->localidade."</td>";
+        $html.="<td>".$consumoAtual->velocidade."</td>";
+        //##############################################
+        // Coluna Atual
+        //##############################################
+        if ($consumoAtual->Percent == '100'){
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
+                                      <span style='font-weight: bold;'>".$consumoAtual->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>>\n";
+        } else if ($consumoAtual->Percent >='70') {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100' style='width: 80%'>
+                                      <span style='font-weight: bold;'>".$consumoAtual->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        } else {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='10' aria-valuemin='0' aria-valuemax='100' style='width: 10%'>
+                                      <span style='font-weight: bold;'>".$consumoAtual->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        }
+        //##############################################
+        // Coluna 01 hora
+        //##############################################
+        if ($consumo1h->Percent == '100'){
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
+                                      <span style='font-weight: bold;'>".$consumo1h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>>\n";
+        } else if ($consumo1h->Percent >='70') {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100' style='width: 80%'>
+                                      <span style='font-weight: bold;'>".$consumo1h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        } else {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='10' aria-valuemin='0' aria-valuemax='100' style='width: 10%'>
+                                      <span style='font-weight: bold;'>".$consumo1h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        }
+        ##############################################
+        // Coluna 06 horas
+        ##############################################
+        if ($consumo6h->Percent == '100'){
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
+                                      <span style='font-weight: bold;'>".$consumo6h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>>\n";
+        } else if ($consumo6h->Percent >='70') {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100' style='width: 80%'>
+                                      <span style='font-weight: bold;'>".$consumo6h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        } else {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='10' aria-valuemin='0' aria-valuemax='100' style='width: 10%'>
+                                      <span style='font-weight: bold;'>".$consumo6h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        }
+        ##############################################
+        // Coluna 24 horas
+        ##############################################
+        if ($consumo24h->Percent == '100'){
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
+                                      <span style='font-weight: bold;'>".$consumo24h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>>\n";
+        } else if ($consumo24h->Percent >='70') {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100' style='width: 80%'>
+                                      <span style='font-weight: bold;'>".$consumo24h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        } else {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='10' aria-valuemin='0' aria-valuemax='100' style='width: 10%'>
+                                      <span style='font-weight: bold;'>".$consumo24h->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        }
+        ##############################################
+        // Coluna 07 dias
+        ##############################################
+        if ($consumo7d->Percent == '100'){
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
+                                      <span style='font-weight: bold;'>".$consumo7d->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>>\n";
+        } else if ($consumo7d->Percent >='70') {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100' style='width: 80%'>
+                                      <span style='font-weight: bold;'>".$consumo7d->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        } else {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='10' aria-valuemin='0' aria-valuemax='100' style='width: 10%'>
+                                      <span style='font-weight: bold;'>".$consumo7d->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        }
+        ##############################################
+        // Coluna 30 dias
+        ##############################################
+        if ($consumo30d->Percent == '100'){
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-danger' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
+                                      <span style='font-weight: bold;'>".$consumo30d->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>>\n";
+        } else if ($consumo30d->Percent >='70') {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100' style='width: 80%'>
+                                      <span style='font-weight: bold;'>".$consumo30d->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        } else {
+              $html .= "  <td>
+                              <div class='progress progress-striped active'>
+                                  <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='10' aria-valuemin='0' aria-valuemax='100' style='width: 10%'>
+                                      <span style='font-weight: bold;'>".$consumo30d->Percent."%</span>
+                                  </div>
+                              </div>
+                          </td>\n";
+        }
+        ##############################################
+        $html.="</tr>";
+      return $html;
     }
 
     public function teste() {
 
-
+    echo $this->table_unidade('UECOMT CAIS DO PORTO');
       // echo $this->router->fetch_class();
       // echo $this->router->fetch_method();
       // echo $this->table_consumo('7 HOUR');
        // $interval =  $this->consumo_model->consumo_atual('7 HOUR');
-       // vd($interval->result_array());
+       // vd($consumo30d->Porcentagem);
+
+       // vd($consumo30d['Porcentagem']);
        // $this->output->enable_profiler(TRUE);
     }
 
--- application/models/Consumo_model.php
+++ application/models/Consumo_model.php
@@ -51,6 +51,33 @@ class Consumo_model extends CI_Model {
     //         ORDER BY Porcentagem DESC
     //         LIMIT 0,100
 
+    public function consumo_unidade($unidade,$interval=null) {
+        $portal_moni = $this->load->database('portalmoni',true);
+        $portal_moni->select('max(periodo_final)')->from('tbl_ebt_consumo_banda');
+        $subQuery = $portal_moni->get_compiled_select();
+        $portal_moni->select('*,date_format(periodo_final,"%d/%m/%Y %H:%m") as periodo_final, ROUND(AVG(Porcentagem), 2)  as Percent');
+        $portal_moni->from('tbl_ebt_consumo_banda');
+        if($interval == null){
+            $portal_moni->where('periodo_final=('.$subQuery.')',null,FALSE);
+        } else {
+            $portal_moni->where('periodo_final between (DATE_SUB(('.$subQuery.'),INTERVAL '.$interval.' ))',null,FALSE);
+            $portal_moni->where('('.$subQuery.')',null,FALSE);
+        }
+        $portal_moni->where('localidade',$unidade);
+        $query = $portal_moni->get();
+        return $query->row();
+    }
+
+
+    // select *,date_format(periodo_final,'%d/%m/%Y %H:%m') as periodo_final,
+    // ROUND(AVG(Porcentagem), 2)  as Porcentagem
+    // from tbl_ebt_consumo_banda
+    // WHERE periodo_final between (DATE_SUB((SELECT MAX(periodo_final)FROM tbl_ebt_consumo_banda ),INTERVAL 1 HOUR ))
+    // and (SELECT MAX(periodo_final) FROM tbl_ebt_consumo_banda )
+    // and localidade = '".$unidade."'
+    // group by localidade
+    // ORDER BY Porcentagem DESC
+    // LIMIT 0,100
 
 }
 
